5f0bbe2799b268fcf16ef5e9b0b05d80007d3df8
Ratatouille <307338947@qq.com>
2019-01-19 23:57:49 +0800

    使用迭代器实现斐波那契数列

--- 02_迭代器斐波那契数列.py
+++ 02_迭代器斐波那契数列.py
@@ -1,45 +1,47 @@
 class Fibonacci(object):
-
-    data_list = [1, 2]
+    data_list = list()
+    fibonacci_list = [1, 2]
 
     def __init__(self, n):
         self.n = n
+        for i in range(0, self.n):
+            self.data_list.append(i)
+        print("---init---", self.data_list)
+        for j in self.data_list:
+            if j == 0:
+                self.fibonacci_list = [0]
+            elif j == 1:
+                self.fibonacci_list = [0, 1]
+            elif j >= 2:
+                fn = self.fibonacci_list[j - 1] + self.fibonacci_list[j - 2]
+                self.fibonacci_list.append(fn)
+        print("---init---", self.fibonacci_list)
 
     def __str__(self):
-        if self.n == 1:
-            return [1]
-        elif self.n == 2:
-            return [1, 2]
-        elif self.n >= 3:
-            while len(self.data_list) < self.n:
-                self.data_list.append()
-
-
-        return self.Fibonacci
+        return str(self.fibonacci_list)
 
     def __iter__(self):
         return IterFibonacci(self)
 
 
-
-
 class IterFibonacci(object):
     def __init__(self, obj):
-        pass
+        self.obj = obj
+        self.i = 0
 
     def __iter__(self):
         pass
 
     def __next__(self):
-        pass
+        while self.i < len(self.obj.fibonacci_list):
+            rtn = self.obj.fibonacci_list[self.i]
+            self.i += 1
+            return rtn
+        else:
+            raise StopIteration
 
 
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    main()
+fibonacci_test = Fibonacci(15)
+print(fibonacci_test)
+for i in fibonacci_test:
+    print(i)
